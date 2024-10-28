b48fd1697c11868cc1624de0578c4595b078cd04
zaryanezrya <ivan@sha.run>
2024-10-28 10:32:08 +0600

    feat: Moving

---
+++ SpaceBattle.Lib/Move.cs
@@ -0,0 +1,28 @@
+﻿namespace SpaceBattle.Lib;
+
+public interface IMoving
+{
+    int[] Position { get; set; }
+    int[] Velocity { get; }
+}
+
+public interface ICommand
+{
+    public void Execute();
+}
+
+public class MoveCommand : ICommand
+{
+    private readonly IMoving obj;
+    public MoveCommand(IMoving obj)
+    {
+        this.obj = obj;
+    }
+    public void Execute()
+    {
+        obj.Position = new int[]{
+            obj.Position[0] + obj.Velocity[0],
+            obj.Position[1] + obj.Velocity[1],
+        };
+    }
+}
---
+++ SpaceBattle.Tests/MoveCommandTest.cs
@@ -0,0 +1,44 @@
+﻿using Moq;
+using SpaceBattle.Lib;
+
+namespace SpaceBattle.Tests;
+
+public class MoveCommandTest
+{
+    [Fact]
+    public void TestPostitive()
+    {
+        // AAA
+        // Arrange
+        //1. Создать движущийся объект
+        var moving = new Mock<IMoving>();
+
+        //2. Движущемуся объекту присвоить позицию и скорость
+        moving.SetupGet(m => m.Position).Returns(new int[] { 12, 5 });
+        moving.SetupGet(m => m.Velocity).Returns(new int[] { -7, 3 });
+
+        // Act
+        // Вызвать команду движения
+        var cmd = new MoveCommand(moving.Object);
+        cmd.Execute();
+
+        // Assert
+        // Проверить, что объект находится в ожидаемой позиции
+        moving.VerifySet(m => m.Position = new int[] { 5, 8 });
+    }
+
+    [Fact]
+    public void TestPositionRaisesException()
+    {
+        var moving = new Mock<IMoving>();
+        moving.SetupGet(m => m.Position).Throws<Exception>();
+        moving.SetupGet(m => m.Velocity).Returns(new int[] { 1, 1 });
+
+        var cmd = new MoveCommand(moving.Object);
+
+        Assert.Throws<Exception>( // Assert
+            () => cmd.Execute() // Act
+        );
+
+    }
+}
